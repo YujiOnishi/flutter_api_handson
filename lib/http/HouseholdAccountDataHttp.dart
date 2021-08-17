b1671f8a92fc354f8c60095d7367b2829b0cc7a9
yuji_onishi <freelancer@y-onishi.net>
2021-08-17 13:48:09 +0900

    スタブの値をランダムに変更

--- lib/http/HouseholdAccountDataHttp.dart
+++ lib/http/HouseholdAccountDataHttp.dart
@@ -1,13 +1,16 @@
 import 'package:api/model/HouseholdAccountData.dart';
+import 'dart:math' as math;
 
 class HouseholdAccountDataHttp {
   static List<HouseholdAccountData> getHouseholdAccountDataList(){
     List<HouseholdAccountData> householdAccountData = [];
+    var random = new math.Random();
     householdAccountData = [
       HouseholdAccountData(1, 0, "カメラ", 20000),
       HouseholdAccountData(2, 1, "売上", 20000),
       HouseholdAccountData(3, 1, "雑収入", 20000),
       HouseholdAccountData(4, 0, "レンズ", 20000),
+      HouseholdAccountData(5, 0, "レンズ", random.nextInt(10000)),
     ];
     return householdAccountData;
   }
